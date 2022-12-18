ec953558bae499d309dee2d5c9077442d1af12cf
IrynaBuchma <buchma.iryna2018@gmail.com>
2022-12-18 16:43:45 +0200

    add slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './js/nav-menu';
 import './js/modal';
 import './js/swiper';
+import './js/gallary';
---
+++ src/js/gallary.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
